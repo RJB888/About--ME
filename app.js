5e32a420dc73293b50257745dfe9c8eef3da6611
kotla12 <kotla9119@gmail.com>
2017-08-08 14:24:01 -0700

    secondQuestion done

--- app.js
+++ app.js
@@ -11,8 +11,17 @@ alert('To answers the questions, please type y for Yes, and n for No');
 // Guessing question # 1
 var firstQuestion = prompt('Do you think that I have a chimpanzee as a pet?');
 if (firstQuestion.toUpperCase() === 'Y' ) {
-  alert('Are you INSANE... these animals are mad lads!');
+  alert('Are you INSANE... those animals are mad lads!');
 }
 else {
   alert('That\'s right, I don\'t think it\'s safe to have one!');
+}
+
+// Guessing question # 2
+var secondQuestion = prompt('Is the country of Somalia located in the Asian contenet between China and Russia?');
+if (secondQuestion.toUpperCase() === 'N' ) {
+  alert('Awesome, looks like you did\'t skip thier Geography class!');
+}
+else {
+  alert('No, you are big time wrong... that\'s why you shouldn\'t skip Geography class');
 }
