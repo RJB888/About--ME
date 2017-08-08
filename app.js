fa3b9e859037a36af9a25214a1b06bcb3f9e640c
kotla12 <kotla9119@gmail.com>
2017-08-08 14:15:57 -0700

    firstQuestion done!

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 'use strict';
-var confirmed = confirm("The Cake is a LIE...Welcome to my About--me page; CLick ok to continue");
-if (confirm === true) {
-  alert("Alrighty, let's do it");
+// a confirm question will pop out
+var confirmed = confirm('This page is a LIE...Welcome to my About--me page; CLick ok to continue');
+if (confirmed === true) {
+  alert('Alrighty, let\'s do it');
 }
 else {
   alert('We are starting anywayz');
+}
+alert('To answers the questions, please type y for Yes, and n for No');
+// Guessing question # 1
+var firstQuestion = prompt('Do you think that I have a chimpanzee as a pet?');
+if (firstQuestion.toUpperCase() === 'Y' ) {
+  alert('Are you INSANE... these animals are mad lads!');
+}
+else {
+  alert('That\'s right, I don\'t think it\'s safe to have one!');
 }
