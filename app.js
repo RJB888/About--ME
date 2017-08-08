2bad0327ad26170219c7405df6eb2f2efb4c5fb2
kotla12 <kotla9119@gmail.com>
2017-08-08 13:52:30 -0700

    added confirm to start the game in app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 'use strict';
+var confirmed = confirm("The Cake is a LIE...Welcome to my About--me page; CLick ok to continue");
+if (confirm === true) {
+  alert("Alrighty, let's do it");
+}
+else {
+  alert('We are starting anywayz');
+}
